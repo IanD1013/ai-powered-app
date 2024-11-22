c5f80bb9c812772fa5b5c9d9f3f24100dffc3cc6
IanD1013 <ian.tung1013@gmail.com>
2024-11-23 11:05:29 +1300

    Functionality done

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -3,16 +3,18 @@
 import { useChat } from 'ai/react';
 
 const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
-  const { messages, handleInputChange, input } = useChat({
-    api: 'api/chat-stream',
+  const { messages, handleInputChange, handleSubmit, input } = useChat({
+    api: '/api/chat-stream',
     body: { sessionId },
   });
 
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
       <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">{JSON.stringify(messages)}</div>
-
-      <input value={input} onChange={handleInputChange} type="text" />
+      <form onSubmit={handleSubmit}>
+        <input className="text-black" value={input} onChange={handleInputChange} type="text" />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
